49e7e463345a25f64d08c412e8bfb186c9144a21
0xBahamoot <lam@autonomous.nyc>
2020-11-23 16:19:05 +0700

    update

--- testsuite/tx_test.go
+++ testsuite/tx_test.go
@@ -131,12 +131,15 @@ func Test_StakeFlow1(t *testing.T) {
 	}
 	monitorPool := func(oldLen1 int, oldLen2 int, oldLen3 int) (bool, int, int, int) {
 		len1 := len(sim.GetBlockchain().BeaconChain.GetShardsWaitingList())
-		len2 := len(sim.GetBlockchain().BeaconChain.GetShardsPendingList())
+		len2 := 0
 		len3 := 0
+		for _, sCommittee := range sim.GetBlockchain().BeaconChain.GetShardsPendingList()["bls"] {
+			len2 += len(sCommittee)
+		}
 		for _, sCommittee := range sim.GetBlockchain().BeaconChain.GetAllCommittees()[sim.GetBlockchain().BeaconChain.GetConsensusType()] {
 			len3 += len(sCommittee)
 		}
-		if oldLen1 != len1 || oldLen2 != len2 || oldLen3 != len3 {
+		if (oldLen1 != len1) || (oldLen2 != len2) || (oldLen3 != len3) {
 			return true, len1, len2, len3
 		}
 		return false, len1, len2, len3
@@ -165,60 +168,34 @@ func Test_StakeFlow1(t *testing.T) {
 
 	_, l1, l2, l3 := monitorPool(0, 0, 0)
 	isChange := false
-	for i := 0; i < 40; i++ {
+	for i := 0; i < 100; i++ {
 		sim.GenerateBlock().NextRound()
 		isChange, l1, l2, l3 = monitorPool(l1, l2, l3)
 		if isChange {
 			fmt.Println("\n----------------------------------")
 			fmt.Println("Beacon Epoch", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetCurrentEpoch())
 			fmt.Println("Beacon Height", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetHeight())
-			sim.GetBlockchain().BeaconChain.GetAllCommittees()
 			viewPool()
 			fmt.Println("----------------------------------")
 			isChange = false
 		}
 	}
-
-	for i := 0; i < 20; i++ {
-		sim.GenerateBlock().NextRound()
-		isChange, l1, l2, l3 = monitorPool(l1, l2, l3)
-		if isChange {
-			fmt.Println("\n----------------------------------")
-			fmt.Println("Beacon Epoch", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetCurrentEpoch())
-			fmt.Println("Beacon Height", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetHeight())
-			sim.GetBlockchain().BeaconChain.GetAllCommittees()
-			viewPool()
-			fmt.Println("----------------------------------")
-			isChange = false
-		}
-	}
-
-	for i := 0; i < 50; i++ {
-		sim.GenerateBlock().NextRound()
-		isChange, l1, l2, l3 = monitorPool(l1, l2, l3)
-		if isChange {
-			fmt.Println("\n----------------------------------")
-			fmt.Println("Beacon Epoch", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetCurrentEpoch())
-			fmt.Println("Beacon Height", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetHeight())
-			sim.GetBlockchain().BeaconChain.GetAllCommittees()
-			viewPool()
-			fmt.Println("----------------------------------")
-			isChange = false
-		}
-	}
-
+	// create miner1 unstake information
 	unstake1 := rpcclient.StopStakingParam{
 		BurnAddr:  sim.GetBlockchain().GetBurningAddress(sim.GetBlockchain().BeaconChain.GetFinalViewHeight()),
 		StakerPrk: sim.GenesisAccount.PrivateKey,
 		MinerPrk:  miner1.PrivateKey,
 	}
+
+	// send stop staking tx
 	if _, err := sim.RPC.API_SendTxStopAutoStake(unstake1); err != nil {
 		panic(err)
 	}
 	fmt.Println("Stopstake staker1 at epoch", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetCurrentEpoch())
-
+	// generate block for tx to add to chain
 	sim.GenerateBlock().NextRound()
 
+	// create miner2 stake information
 	miner2 := sim.NewAccountFromShard(0)
 	minerCm2, _ := account.BuildCommitteePubkey(miner2.PrivateKey, sim.GenesisAccount.PaymentAddress)
 	stake2 := rpcclient.StakingTxParam{
@@ -231,8 +208,9 @@ func Test_StakeFlow1(t *testing.T) {
 		StakeShard:   true,
 		AutoRestake:  true,
 	}
-
 	stakeList = append(stakeList, stake2)
+
+	// send miner2 staking information
 	_, err = sim.RPC.API_SendTxStaking(stake2)
 	if err != nil {
 		panic(err)
@@ -246,7 +224,6 @@ func Test_StakeFlow1(t *testing.T) {
 			fmt.Println("\n----------------------------------")
 			fmt.Println("Beacon Epoch", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetCurrentEpoch())
 			fmt.Println("Beacon Height", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetHeight())
-			sim.GetBlockchain().BeaconChain.GetAllCommittees()
 			viewPool()
 			fmt.Println("----------------------------------")
 			isChange = false
@@ -258,21 +235,12 @@ func Test_StakeFlow1(t *testing.T) {
 		panic(err)
 	}
 
-	for i := 0; i < 300; i++ {
+	for i := 0; i < 5; i++ {
 		sim.GenerateBlock().NextRound()
-		isChange, l1, l2, l3 = monitorPool(l1, l2, l3)
-		if isChange {
-			fmt.Println("\n----------------------------------")
-			fmt.Println("Beacon Epoch", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetCurrentEpoch())
-			fmt.Println("Beacon Height", sim.GetBlockchain().BeaconChain.GetBestView().GetBlock().GetHeight())
-			sim.GetBlockchain().BeaconChain.GetAllCommittees()
-			viewPool()
-			fmt.Println("----------------------------------")
-			isChange = false
-		}
 	}
-	viewPool()
-	fmt.Println()
+
+	bl1, _ := sim.RPC.API_GetBalance(miner1)
+	fmt.Println(bl1)
 }
 
 func Test_PDEFlow(t *testing.T) {
