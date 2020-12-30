99599c6afbd2245906998c2838a6aca138c3272e
0xkumi <dung.q@autonomous.nyc>
2020-12-30 15:31:13 +0700

    add netmonitor

---
+++ dev/netmonitor/bftmonitor.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/incognitochain/incognito-chain/wire"
+	"regexp"
+	"strconv"
+)
+
+//this monitor
+//bft block fork
+//bft timing
+
+type bftmonitor struct {
+	chainName string
+	chainID int
+}
+
+func NewBFTMonitor(chainName string) (b *bftmonitor,err error) {
+	chainID := -1
+	if chainName != "beacon" {
+		regex,_ := regexp.Compile(`shard-(\d)`)
+		str := regex.FindStringSubmatch(chainName)
+		chainID, err = strconv.Atoi(str[1])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &bftmonitor{
+		chainName,
+		chainID,
+	}, nil
+}
+
+func (s *bftmonitor) receiveBFTMsg(msg *wire.MessageBFT){
+	fmt.Println("chain", s.chainID, "receive message", string(msg.Content), msg.)
+	//if msg.Type == "propose" {
+	//
+	//}
+	//if msg.Type == "vote" {
+	//
+	//}
+}
+
+
+
+
--- dev/netmonitor/main.go
+++ dev/netmonitor/main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"fmt"
 	devframework "github.com/0xkumi/incognito-dev-framework"
+	"github.com/incognitochain/incognito-chain/wire"
 )
+
+var BFTMonitorProcesses = make(map[string]*bftmonitor)
 
 func main() {
 	network := devframework.NewNetworkMonitor("127.0.0.1:9330")
+
 	network.OnReceive(devframework.MSG_BFT, func (msg interface{}){
 		switch v := msg.(type) {
+		case *wire.MessageBFT:
+			if _,ok := BFTMonitorProcesses[v.ChainKey]; !ok {
+				b ,err := NewBFTMonitor(v.ChainKey)
+				BFTMonitorProcesses[v.ChainKey] = b
+				//fmt.Println("set bft", b, err)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			}
+			BFTMonitorProcesses[v.ChainKey].receiveBFTMsg(v)
 		default:
-			fmt.Println("receive message", v)
+			fmt.Println("receive message", v, msg)
 		}
 	})
 	select {}
